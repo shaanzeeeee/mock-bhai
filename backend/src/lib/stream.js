07ba2a1cd328b65b2d504748259975314f545c91
shaanzeeeee <mahinuzzaman.shaan@gmail.com>
2025-11-20 20:50:50 +0600

    add stream-chat dependency and initialize stream.js

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,34 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env.js";
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+    throw new Error("Stream API key and secret must be defined in environment variables");
+}
+
+export const chatClient = StreamChat(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await chatClient.upsertUser(userData);
+        console.log(`Stream user with ID ${userData.id} upserted successfully.`);
+    } catch (error) {
+        console.error("Error upserting Stream user:", error);
+    }
+}
+
+
+export const deleteStreamUser = async (userId) => {
+    try {
+        await chatClient.deleteUser(userId);
+        console.log(`Stream user with ID ${userId} deleted successfully.`);
+    } catch (error) {   
+        console.error("Error deleting Stream user:", error);
+    }
+};
+
+
+
+//todo: add another function
